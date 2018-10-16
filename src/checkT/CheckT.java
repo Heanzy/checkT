1f4bd350ab2f74f947ea69e4b469fa6cca5c4676
Heanzy <289139701@qq.com>
2018-10-16 23:57:00 +0800

    计算map

--- src/checkT/CheckT.java
+++ src/checkT/CheckT.java
@@ -4,6 +4,8 @@ import java.security.KeyStore.Entry;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.swing.JTable;
+
 public class CheckT {
 	private double b0;
 	private double b1;
@@ -17,12 +19,12 @@ public class CheckT {
 	private double S;
 	private int n;
 	private double sB1;
+	private int numOfDataPreGroup;
+	private int numOfTimePoint;
 	public CheckT() {
 		// TODO 自动生成的构造函数存根
 	}
-	public ArrayList<Double> getAveragex() {
-		return averagex;
-	}
+
 	public void setsB1(){
 		double sumUp = S;
 		double sumDown = 0d;
@@ -89,7 +91,42 @@ public class CheckT {
             averagex.add((double)(sum/E.size()));
         }
     }
+	public void setTimeAndData(JTable table) throws Exception{
+		int start [] = FindTableDataStart(table);
+		for(int i = start[0];i < start[0] + numOfTimePoint;i++){
+			for(int j = start[1]+1;j < start[1] + numOfDataPreGroup;j++)
+				Double sum =0d;
+			
+		}
+			
+	}
+	public int [] FindTableDataStart(JTable table)throws Exception {
+		int [] start =new int[2];
+		int column = table.getColumnCount();
+		int row = table.getRowCount();
+		int N = 0;
+		boolean isFound = false;
+		for(int i = 0;i<row&&!isFound;i++)
+			for(int j = 0;j<column&&!isFound;j++){
+				if(table.getValueAt(i, j)!=""){
+					start[0]=i;
+					start[1]=j;
+					isFound = true;
+				}
+			}
+		System.out.println("起始坐标是："+start[0]+start[1]);	
+		if(!isFound)
+			throw new Exception();
+		
+		return start;
+	    }
 	public boolean isPass(){
 		return false;
+	}
+	public void setNumOfGroup(int numOfGroup) {
+		this.numOfGroup = numOfGroup;
+	}
+	public int getNumOfGroup() {
+		return numOfGroup;
 	}
 }
